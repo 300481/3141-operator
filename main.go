3d2767cf19e337e5d4541553b30cf0d42bd26fef
300481 <300481@300481.de>
2019-08-21 12:22:50 +0200

    add newLocalRepo function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -10,6 +11,8 @@ import (
 	"strings"
 
 	notification "github.com/300481/3141-notification"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/300481/mq"
@@ -22,9 +25,50 @@ type Operator struct {
 	Args     []string
 }
 
+type LocalRepo struct {
+	Path string
+}
+
 const (
 	errMissingCommand = "Required environment variable 'COMMAND' not specified."
 )
+
+// newLocalRepo clones a remote Git repo and checks the commit out
+func newLocalRep(url, commit string) (repo *LocalRepo, err error) {
+	workdir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	tempdir, err := ioutil.TempDir(workdir, "")
+	if err != nil {
+		return nil, err
+	}
+
+	repository, err := git.PlainClone(tempdir, false, &git.CloneOptions{URL: url})
+	if err != nil {
+		return nil, err
+	}
+
+	worktree, err := repository.Worktree()
+	if err != nil {
+		return nil, err
+	}
+
+	err = worktree.Checkout(&git.CheckoutOptions{
+		Hash: plumbing.NewHash(commit),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	repo = &LocalRepo{
+		Path: tempdir,
+	}
+
+	err = nil
+	return repo, err
+}
 
 // new creates new Operator struct
 func new() (op *Operator, err error) {
