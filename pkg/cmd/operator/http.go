18bf4c4b5c3ffed61eb3dba53b05a92c6a9a79eb
300481 <300481@300481.de>
2019-11-11 23:17:49 +0100

    add github hook handler

--- pkg/cmd/operator/http.go
+++ pkg/cmd/operator/http.go
@@ -4,13 +4,46 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"gopkg.in/go-playground/webhooks.v5/github"
 )
 
 func (o *Operator) routes() {
-	o.Router.HandleFunc("/", o.hookHandler)
+	o.Router.HandleFunc("/github", o.githubHookHandler)
 }
 
-func (o *Operator) hookHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("hook.handler:", r.Method, "request from ", r.RemoteAddr)
-	io.WriteString(w, "")
+func (o *Operator) githubHookHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("githubhook.handler:", r.Method, "request from ", r.RemoteAddr)
+
+	// get the Secret from environment
+	secret := os.Getenv("GITHUB_SECRET")
+
+	// Create a new hook config (with secret)
+	hook, err := github.New(github.Options.Secret(secret))
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+
+	// parse the hooks payload
+	payload, err := hook.Parse(r, github.Event("push"))
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	pushMessage := payload.(github.PushPayload)
+
+	log.Println("Got Payload from:", pushMessage.Repository)
+
+	// respond OK
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "{}")
+}
+
+// error handling function
+func handleError(err error, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(err.Error()))
+	log.Printf("error: %s", err.Error())
 }
